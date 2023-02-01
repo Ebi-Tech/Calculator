60867fbb287913741d08106cdc2da77f6c47a86e
Divine Ebube Ifechukwude <difebi14@gmail.com>
2023-02-01 13:32:49 +0100

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,29 +1,31 @@
-// select all the buttons
-const buttons = document.querySelectorAll('button');
-// select the <input type="text" class="display" disabled> element
-const display = document.querySelector('.display');
+// targeted elements by id and assigned into variables ( we’ll use these variables later in tutorial to print calculation text and result text )
+const result = document.getElementById("resultText");
+const calculation = document.getElementById("calculationText");
 
-// add eventListener to each button
-buttons.forEach(function(button) {
-  button.addEventListener('click', calculate);
+/* function to insert number onclick */
+function insert(num) {
+  calculation.textContent += num;
+  console.log(num);
+}
+
+// targeting clear button by id and assigning a blank value to the calculation and 0 to result on click event listener
+document.getElementById("clear").addEventListener("click", function () {
+  calculation.textContent = "";
+  result.textContent = 0;
 });
 
-// calculate function
-function calculate(event) {
-  // current clicked buttons value
-  const clickedButtonValue = event.target.value;
+/* on click event listener targeting equal button by id then 
+- assigning the calculation to the exp variable (we'll use this variable to evaluates calculation using eval method)
+- executing exp variable using the eval method and assigning to the result to result screen 
+- assigning the output of the result to the calculation to print result to calculation screen too  */
+document.getElementById("equal").addEventListener("click", function () {
+  var exp = calculation.textContent;
+  result.textContent = eval(exp);
+  calculation.textContent = result.textContent;
+});
 
-  if (clickedButtonValue === '=') {
-    // check if the display is not empty then only do the calculation
-    if (display.value !== '') {
-      // calculate and show the answer to display
-      display.value = eval(display.value);
-    }
-  } else if (clickedButtonValue === 'C') {
-    // clear everything on display
-    display.value = '';
-  } else {
-    // otherwise concatenate it to the display
-    display.value += clickedButtonValue;
-  }
-}
+// targeting back key button by id and assign value of calculation to exp variable then assign calculation by substring exp variable from 0 to (length of exp -1 )
+document.getElementById("back").addEventListener("click", function () {
+  var exp = calculation.textContent;
+  calculation.textContent = exp.substring(0, exp.length - 1);
+});
